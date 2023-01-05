fd8931018ace5e69108bfd62a5ebccb21039c8ee
Emiliano Pollero <emiliano.pollero.work@gmail.com>
2023-01-05 16:49:05 +0100

    Add docker-compose.yaml with selenium grid and refactor code to work with it

--- src/test/java/com/newtours/tests/BookFlightTest.java
+++ src/test/java/com/newtours/tests/BookFlightTest.java
@@ -1,69 +1,57 @@
 package com.newtours.tests;
 
 import com.newtours.pages.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import com.tests.BaseTest;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class BookFlightTest {
+public class BookFlightTest extends BaseTest {
 
-    private WebDriver driver;
     private String noOfPassengers;
     private String expectedPrice;
 
     @BeforeTest
     @Parameters({"noOfPassengers", "expectedPrice"})
-    public void setupDriver(String noOfPassengers, String expectedPrice){
+    public void setupParameters(String noOfPassengers, String expectedPrice) {
         this.noOfPassengers = noOfPassengers;
         this.expectedPrice = expectedPrice;
-        System.setProperty("webdriver.chrome.driver", "/home/emiliano/Downloads/chromedriver_linux64/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.setHeadless(true);
-        this.driver = new ChromeDriver(options);
-
     }
 
     @Test
-    public void registrationPageTest(){
+    public void registrationPageTest() {
         RegistrationPage registrationPage = new RegistrationPage(driver);
         registrationPage.goTo();
         registrationPage.enterUserDetails("selenium", "docker");
         registrationPage.enterUserCredentials("selenium", "docker");
         registrationPage.submit();
     }
+
     @Test(dependsOnMethods = "registrationPageTest")
-    public void registrationConfirmationPageTest(){
+    public void registrationConfirmationPageTest() {
         RegistrationConfirmationPage registrationConfirmationPage = new RegistrationConfirmationPage(driver);
         registrationConfirmationPage.goToFlightsDetailsPage();
     }
 
     @Test(dependsOnMethods = "registrationConfirmationPageTest")
-    public void flightDetailsPageTest(){
+    public void flightDetailsPageTest() {
         FlightDetailsPage flightDetailsPage = new FlightDetailsPage(driver);
         flightDetailsPage.selectPassengers(noOfPassengers);
         flightDetailsPage.goToFindFlightsPage();
     }
 
     @Test(dependsOnMethods = "flightDetailsPageTest")
-    public void findFlightPageTest(){
+    public void findFlightPageTest() {
         FindFlightsPage findFlightsPage = new FindFlightsPage(driver);
         findFlightsPage.submitFindFlightPage();
         findFlightsPage.goToFlightConfirmationPage();
     }
 
     @Test(dependsOnMethods = "findFlightPageTest")
-    public void flightConfirmationPageTest(){
+    public void flightConfirmationPageTest() {
         FlightConfirmationPage flightConfirmationPage = new FlightConfirmationPage(driver);
         String actualPrice = flightConfirmationPage.getPrice();
         Assert.assertEquals(actualPrice, expectedPrice);
-    }
-    @AfterTest
-    public void quitBrowser(){
-        this.driver.quit();
     }
 }
--- src/test/java/com/searchmodule/tests/SearchTest.java
+++ src/test/java/com/searchmodule/tests/SearchTest.java
@@ -1,27 +1,12 @@
 package com.searchmodule.tests;
 
 import com.searchmodule.pages.SearchPage;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import com.tests.BaseTest;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class SearchTest {
-
-    private WebDriver driver;
-
-    @BeforeTest
-    public void setupDriver() {
-        System.setProperty("webdriver.chrome.driver", "/home/emiliano/Downloads/chromedriver_linux64/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.setHeadless(true);
-        this.driver = new ChromeDriver(options);
-    }
-
+public class SearchTest extends BaseTest {
     @Test
     @Parameters({"keyword"})
     public void search(String keyword) {
@@ -32,10 +17,5 @@ public class SearchTest {
         int size = searchPage.getResult();
         System.out.println(size);
         Assert.assertTrue(size > 0);
-    }
-
-    @AfterTest
-    public void quitDriver() {
-        this.driver.quit();
     }
 }
